43b74bea13d192eb431abbb1d40a72d1c7ba8038
Guilhem <gpoties@gmail.com>
2023-11-21 11:40:57 +0100

    Ajout d'une version incomplète de mosaique

--- src/workshop.cpp
+++ src/workshop.cpp
@@ -101,7 +101,25 @@ void cercle(sil::Image image);
 
 void rosace(sil::Image image);
 
-void mosaique(sil::Image image);
+void mosaique(sil::Image image) {
+    /*
+    int repetition {5};
+    sil::Image nouvelleImage {image.width(), image.height()*repetition};
+
+    for (int i{0}; i < repetition^2; i++)
+    {
+        for (int x{0}; x < image.width()/2; x++)
+        {
+            for (int y{0}; y < image.height(); y++)
+            {
+                nouvelleImage.pixel(x + ((image.width()*i)%repetition), y + ((image.height()*i)%repetition)) = image.pixel(x,y);
+            }
+        }
+    }
+
+    nouvelleImage.save("output/mosaique.png");
+    */
+}
 
 void mosaiqueMiroir(sil::Image image);
 
